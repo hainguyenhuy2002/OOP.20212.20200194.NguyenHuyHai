63808b307597581d558f0b92eae7481a4e7b7330
hainguyenhuy2002 <81941497+hainguyenhuy2002@users.noreply.github.com>
2022-05-07 10:49:20 +0700

    files for dev

---
+++ lab03/AimsProject/Cart.java
@@ -0,0 +1,9 @@
+public class Cart {
+    public static final int MAX_NUMBERS_OREDERD = 20;
+    private int qtyOrdered = 0;
+    private DigitalVideoDisc itemsOrdered[] = new 
+    DigitalVideoDisc[MAX_NUMBERS_OREDERD];
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        
+    }
+}
