a15e375ee4ae9384469958996971339b59b734ba
hainguyenhuy2002 <81941497+hainguyenhuy2002@users.noreply.github.com>
2022-06-13 22:13:38 +0700

    final submission for lab 06

--- AimsProject/AimsProject/src/hust/soict/dsai/test/media/BookTest.java
+++ AimsProject/AimsProject/src/hust/soict/dsai/test/media/BookTest.java
@@ -5,9 +5,9 @@ import hust.soict.dsai.aims.media.Book;
 public class BookTest {
 
 	public static void main(String[] args) {
-		Book book = new Book("Life_Of_Pi", "Mystery", 5.6f, "This book is about the life and adventure of Pi, and also about animals aswell!");
-		book.addAuthor("Yan Martel");
-		book.addAuthor("Piscine Patel");
+		Book book = new Book("Life_Of_Pi", "Mystery", 5.6f, "Yolo, this is a title naruto, sauke, obitooooo");
+		book.addAuthor("Sea");
+		book.addAuthor("TV");
 
 		System.out.println(book.toString());
 		System.out.println(book.getContentTokens());
